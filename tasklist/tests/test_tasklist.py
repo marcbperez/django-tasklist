acdd34b449edf5cfd1a347f7a6e576f78ae50675
marcbperez <marcbperez@users.noreply.github.com>
2017-04-06 17:41:39 +0100

    Changed Python tests for this module.

--- tasklist/tests/test_tasklist.py
+++ tasklist/tests/test_tasklist.py
@@ -3,17 +3,9 @@ from django.urls import reverse
 from django.conf import settings
 
 
-def module_url(url):
-    prefix = ''
-
-    if settings.ROOT_URLCONF != 'tasklist.urls':
-        prefix = 'tasklist:'
-
-    return url + prefix
-
-
 class TasklistViewTests(TestCase):
+    url_prefix = "" if settings.ROOT_URLCONF == "tasklist.urls" else "tasklist:"
+
     def test_index_view(self):
-        url = module_url('index')
-        response = self.client.get(reverse(url))
+        response = self.client.get(reverse(self.url_prefix + 'index'))
         self.assertEqual(response.status_code, 200)
